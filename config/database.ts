bb76df8e6b8e2208a0217061e92c6b2ec6f330a6
GodswillUdida <udidagodswill7@gmail.com>
2025-02-16 05:01:30 +0100

    database

--- config/database.ts
+++ config/database.ts
@@ -2,15 +2,15 @@ import { connect } from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const dataBase = async() => {
+export const dataBase = async () => {
   try {
-   await connect(process.env.MONGO_URI || "mongodb://localhost:27017/fintech")
-     .then(() => {
-       console.log("Connected to MongoDB");
-     })
-     .catch((err) => {
-       console.error("Failed to connect to MongoDB", err);
-     });
+    await connect(process.env.MONGO_URI || "mongodb://localhost:27017/fintech")
+      .then(() => {
+        console.log("Connected to MongoDB");
+      })
+      .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+      });
   } catch (error: any) {
     console.log(`Error conecting to database: ${error.message}`);
   }
