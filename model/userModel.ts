03bc852ca98dc20478332fc7856b1f4fd917e1f1
GodswillUdida <udidagodswill7@gmail.com>
2025-02-19 03:34:44 +0100

    cleaning comments

--- model/userModel.ts
+++ model/userModel.ts
@@ -1,11 +1,9 @@
 import { model, Schema } from "mongoose";
-// import argon2 from "argon2";
 
 export interface IUser extends Document {
   username: string;
   password: string;
   email: string;
-  // comparePassword: (candidatePassword: string) => Promise<boolean>;
   createdAt: Date;
   updatedAt: Date;
 }
@@ -20,37 +18,6 @@ const UserSchema = new Schema<IUser>(
     timestamps: true,
   }
 );
-
-// , {
-    //   type: argon2.argon2id,
-    //   memoryCost: 2 ** 16, 
-    //   timeCost: 3,
-    //   parallelism: 2, 
-    // });
-
-// UserSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-//   try {
-//     this.password = await argon2.hash(this.password);
-//     next();
-//   } catch (error:any) {
-//     next(error.message);
-//   }
-// });
-
-// : Promise<boolean>
-
-// UserSchema.methods.comparePassword = async function (
-//   candidatePassword: string
-// ) {
-//   return await argon2.verify(this.password, candidatePassword);
-// };
-
-// UserSchema.methods.toJSON = function () {
-//   const user = this.toObject();
-//   delete user.password;
-//   return user;
-// };
 
 const User = model<IUser>('User', UserSchema);
 export default User;
